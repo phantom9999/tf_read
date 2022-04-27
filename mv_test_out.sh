321ec37d741f84788f8071fa5be7fcdefd2ea63a
phantom9999 <zhangxiao9999@gmail.com>
2022-04-27 02:42:37 +0000

    mv test to test_dir

---
+++ mv_test_out.sh
@@ -0,0 +1,13 @@
+
+
+for filename in `find . -name '*_test.cc'`
+do
+    echo ${filename};
+    origin_dir=`dirname ${filename}`;
+    target_dir="test_dir/${origin_dir}";
+    echo ${target_dir};
+    mkdir -p ${target_dir};
+    cp ${filename} ${target_dir};
+done
+
+
